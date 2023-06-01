601e4c216a83d088b515272f9f4434848e59dba7
henrike32 <henrique.lambertucci@gmail.com>
2023-05-31 20:00:12 -0400

    will do more work soon

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,12 +13,16 @@
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their name capitalized.
 
 // a) Create a test with an expect statement using the variable provided.
-
-const hitchhikersCharacters = [
-  { name: "ford prefect", occupation: "a hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
+describe("greeter", () => {
+  const hitchhikersCharacters = [
+    { name: "ford prefect", occupation: "a hitchhiker" },
+    { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+    { name: "arthur dent", occupation: "a radio employee" }
+  ]
+  it("returns a generic greeting", () => {
+    expect(greeter()).toEqual("Hello, LEARN student!")
+  })
+})
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
 
 // b) Create the function that makes the test pass.
@@ -27,20 +31,33 @@ const hitchhikersCharacters = [
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("greeter", () => {
+  const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+  // Expected output: [ 2, 0, -1, 0 ]
+  const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+  // Expected output: [ 2, 1, -1 ]
+  it("returns a generic greeting", () => {
+    expect(greeter()).toEqual("Hello, LEARN student!")
+  })
+})
+
+
 
 // b) Create the function that makes the test pass.
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
+
+describe("greeter", () => {
+  const cubeAndSum1 = [2, 3, 4]
+  // Expected output: 99
+  const cubeAndSum2 = [0, 5, 10]
+  // Expected output: 1125
+  it("returns a generic greeting", () => {
+    expect(greeter()).toEqual("Hello, LEARN student!")
+  })
+})
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
 
 // b) Create the function that makes the test pass.
